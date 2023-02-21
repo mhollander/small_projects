d8fc4fe2c07c2ecbb45f5b4f19266b1b1af36695
Michael Hollander <hollander@gmail.com>
2023-02-21 11:26:34 -0500

    update graphics, improve look and feel, make function and repeat with outputs for diffn't shootings.

--- shooting_victims_recent_lookback.R
+++ shooting_victims_recent_lookback.R
@@ -26,23 +26,114 @@ rolling_victims <- function(df) {
            nfs_90_day = nfs_3,
     ) %>% 
     pivot_longer(names_to = "name", values_to = "value",  cols = -date_) %>% 
-    mutate(type = if_else(grepl("^fs", name), "Fatal", "Non-Fatal"))
+    mutate(type = if_else(grepl("^fs", name), "Fatal", "Non-Fatal"),
+           name = factor(name, levels = c("fs", "fs_7_day", "fs_30_day", "fs_90_day", "nfs", "nfs_7_day", "nfs_30_day", "nfs_90_day")))
 }
 
-shooting_victims %>% 
-  rolling_victims() %>% 
-  group_by(name) %>% 
-  mutate(prepandemic_avg = mean(value[date_ <= "2020-03-11"], na.rm = T)) %>% 
-  ungroup() %>% 
-  ggplot(aes(x = as.Date(date_), y = value, color = name)) + 
-  geom_point() +
-  geom_hline(data = . %>% distinct(prepandemic_avg, name, .keep_all = T), aes(yintercept = prepandemic_avg, color = name)) +
-  geom_vline(aes(xintercept = ymd("2020-03-11")), color = "red", linetype = "dashed", alpha = .5) +
-  annotate("text", x = ymd("2020-03-11"), y = Inf, angle = 90, label = "COVID-19 Pandemic Declared", vjust = "left", hjust = "right", alpha = .5) + 
-  facet_wrap(~type, scales = "free_y") +
-  labs(title = "Fatal and Non-Fatal Shootings in Philadelphia",
-       subtitle = "1-, 7-, 30-, and 90-day running totals",
-       x = "", y = "") 
+rolling_shooting_victims_plot <- function(df, ...) {
+  
+  
+  df <- df %>% 
+    group_by(name) %>%   mutate(prepandemic_avg = mean(value[date_ <= "2020-03-11"], na.rm = T)) %>% 
+    ungroup() 
+  
+  max_y <- df %>% pull(value) %>% max(na.rm = T)
+  max_avg <- df %>% distinct(prepandemic_avg) %>% pull() %>% max(na.rm = T)
+  
+  df %>%
+    ggplot(aes(x = as.Date(date_), y = value, color = name)) +
+    geom_point(alpha = .5, size = .8) +
+    geom_hline(data = . %>% distinct(prepandemic_avg, name, .keep_all = T), 
+               aes(yintercept = prepandemic_avg, color = name)) +
+    geom_vline(aes(xintercept = ymd("2020-03-11")), color = "red", linetype = "dashed", alpha = .5) +
+    annotate("text", x = ymd("2020-02-15"), y = Inf, angle = 90, label = "COVID-19 Pandemic Declared", vjust = "left", hjust = "right", alpha = .5) +
+    annotate("text", x = ymd("2016-01-01"), y = .8*max_y, alpha = .5, label = "Pre-pandemic average", vjust = "top", hjust = "left") +
+    annotate("curve", x = ymd("2016-10-01"), xend = ymd("2019-01-01"), y = .77*max_y, yend = max_avg * 1.01,
+             arrow = arrow(length = unit(0.02, "npc")), curvature = -.35, alpha = .5) +
+    scale_color_discrete(labels = c("fs" = "Daily", "nfs" = "Daily", "fs_7_day" = "7 day", "fs_30_day" = "30 day",
+                                    "fs_90_day" = "90 day", "nfs_7_day" = "7 day", "nfs_30_day" = "30 day",
+                                    "nfs_90_day" = "90 day")) + 
+    scale_x_date(expand = c(.01, .01)) +
+    labs(...,
+         x = "", y = "", color = "Shooting Count: ") +
+    theme_minimal() + 
+    theme(plot.title = element_text(size = 20, face = "bold"),
+          plot.subtitle = element_text(size = 17, face = "bold"),
+          axis.text = element_text(size = 16),
+          axis.ticks = element_blank(),
+          legend.position = "top",
+          legend.text = element_text(size = 14),
+          legend.title = element_text(size = 14, face = "bold"))
+}
+
+rolling_shooting_victims_plot(shooting_victims %>%
+                                rolling_victims() %>% 
+                                filter(grepl("^fs", name)), 
+                              title = "Fatal Shootings in Philadelphia",
+                              subtitle = "1-, 7-, 30-, and 90-day running totals") 
+ggsave("Fatal_90_day.png", width = 9, height = 7, bg = "white")
+
+rolling_shooting_victims_plot(shooting_victims %>%
+                                rolling_victims() %>% 
+                                filter(grepl("^fs", name),
+                                       !grepl("90", name)), 
+                              title = "Fatal Shootings in Philadelphia",
+                              subtitle = "1-, 7-, and 30- day running totals")
+
+ggsave("Fatal_30_day.png", width = 9, height = 7, bg = "white")
+
+rolling_shooting_victims_plot(shooting_victims %>%
+                                rolling_victims() %>% 
+                                filter(grepl("^fs", name),
+                                       !grepl("90|30", name)), 
+                              title = "Fatal Shootings in Philadelphia",
+                              subtitle = "1- and 7-day running totals") 
+ggsave("Fatal_7_day.png", width = 9, height = 7, bg = "white")
+
+rolling_shooting_victims_plot(shooting_victims %>%
+                                rolling_victims() %>% 
+                                filter(grepl("^fs", name),
+                                       !grepl("90|30|7", name)), 
+                              title = "Fatal Shootings in Philadelphia",
+                              subtitle = "Single Day Counts") + 
+  theme(legend.position = "none")
+
+ggsave("Fatal_daily.png", width = 9, height = 7, bg = "white")
+
+rolling_shooting_victims_plot(shooting_victims %>%
+                                rolling_victims() %>% 
+                                filter(grepl("^nfs", name)), 
+                              title = "Non-Fatal Shootings in Philadelphia",
+                              subtitle = "1-, 7-, 30-, and 90-day running totals") 
+ggsave("non-fatal_90_day.png", width = 9, height = 7, bg = "white")
+
+rolling_shooting_victims_plot(shooting_victims %>%
+                                rolling_victims() %>% 
+                                filter(grepl("^nfs", name),
+                                       !grepl("90", name)), 
+                              title = "Non-Fatal Shootings in Philadelphia",
+                              subtitle = "1-, 7-, and 30- day running totals")
+
+ggsave("non-fatal_30_day.png", width = 9, height = 7, bg = "white")
+
+rolling_shooting_victims_plot(shooting_victims %>%
+                                rolling_victims() %>% 
+                                filter(grepl("^nfs", name),
+                                       !grepl("90|30", name)), 
+                              title = "Non-Fatal Shootings in Philadelphia",
+                              subtitle = "1- and 7-day running totals") 
+ggsave("non-fatal_7_day.png", width = 9, height = 7, bg = "white")
+
+rolling_shooting_victims_plot(shooting_victims %>%
+                                rolling_victims() %>% 
+                                filter(grepl("^nfs", name),
+                                       !grepl("90|30|7", name)), 
+                              title = "Non-Fatal Shootings in Philadelphia",
+                              subtitle = "Single Day Counts") + 
+  theme(legend.position = "none")
+
+ggsave("non-fatal_daily.png", width = 9, height = 7, bg = "white")
+
 
 
 shooting_victims %>% 
